a9268d1cb2c4a1a3ff43bae6fb35fd4ba1f41be6
Cullen Kirkpatrick <cjk101812@gmail.com>
2016-02-27 19:15:48 -0500

    Adding routes, also added mongo service to stay up on AWS

--- simpleServer.js
+++ simpleServer.js
@@ -4,44 +4,78 @@ var app      = express();
 
 //ESTABLISH MONGO CONNECTION
 var mongoose = require('mongoose');
-var testdb = mongoose.createConnection('mongodb://52.90.30.153/test');
+var testdb = mongoose.createConnection('mongodb://54.164.79.75/test');
 
-var testSchema = mongoose.Schema({
-    test: String,
-		user: String
-});
 
+//==============USER DATA=============//
 var userSchema = mongoose.Schema({
 		username: String,
 		password: String,
 		fullname: String
-})
-
-var testModel = testdb.model('Test', testSchema, 'Test');
-
+});
 var userModel = testdb.model('Users', userSchema, 'Users');
+
+//==============MOVEMENTS============//
+var movementSchema = mongoose.Schema({
+		name: String,
+		category: String,
+		subcategory: String,
+    pr: String
+});
+var movementModel = testdb.model('Movements', movementSchema, 'Movements');
+
+//==============WORKOUTS=============//
+var workoutSchema = mongoose.Schema({
+		date: String,
+		name: String,
+    type: String,
+    movements: {
+      name: String,
+  		category: String,
+  		subcategory: String,
+      repcount: String,
+      weight: String
+    },
+    repscheme: String,
+    rounds: String,
+    result: String
+});
+var workoutModel = testdb.model('Workouts', workoutSchema, 'Workouts');
+
+
 
 //=======================================//
 //=========SET UP SOME ROUTES============//
 //=======================================//
-//Test Route
+//============Test Route=================//
 app.get('/uh', function(req, res) {
     res.send('Hello World');
 });
 
-app.get('/gettests', function(req, res) {
-    // use mongoose to get all todos in the database
-    testModel.find({}, function(error, data){
-        res.json(data);
-    });
-});
-
+//============USER ROUTES================//
 app.get('/getusers', function(req, res) {
     // use mongoose to get all todos in the database
     userModel.find({}, function(error, data){
         res.json(data);
     });
 });
+
+//==========MOVEMENT ROUTES=============//
+app.get('/getmovements', function(req, res) {
+    // use mongoose to get all todos in the database
+    movementModel.find({}, function(error, data){
+        res.json(data);
+    });
+});
+
+//===========WORKOUT ROUTES=============//
+app.get('/getworkouts', function(req, res) {
+    // use mongoose to get all todos in the database
+    workoutModel.find({}, function(error, data){
+        res.json(data);
+    });
+});
+
 
 app.all('/*', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
